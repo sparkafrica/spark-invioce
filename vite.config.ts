a2dc4b7c8674fb57c2ddcb3ac1155633aed96482
kingsonseang <kingsonseang@gmail.com>
2026-09-11 00:18:32 +0100

    fix: remove nitro from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 // import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import { nitro } from 'nitro/vite';
+// import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
@@ -12,27 +12,27 @@ const config = defineConfig({
 	ssr: { noExternal: [] },
 	plugins: [
 		devtools(),
-		nitro({
-			// preset: 'bun',
-			rollupConfig: { external: [/^@sentry\//, '@react-pdf/renderer'] },
-			routeRules: {
-				// Cache dynamic-but-slow-changing routes
-				'/activity/**': { swr: 300 }, // 5 min
-				'/clients/**': { swr: 300 }, // 5 min
-				'/dashboard/**': { swr: 300 }, // 5 min
-				'/invoices/**': { swr: 300 }, // 5 min
-				'/memos/**': { swr: 300 }, // 5 min
-				'/settings/**': { swr: 300 }, // 5 min
-				'/team/**': { swr: 300 }, // 5 min
-				'/products/**': { swr: 300 }, // 5 min
-				'/api/**': { swr: 60 }, // 1 min for API routes if needed
-			},
-			// output: {
-			// 	dir: 'dist',
-			// 	serverDir: 'dist/server',
-			// 	publicDir: 'dist/public',
-			// },
-		}),
+		// nitro({
+		// 	// preset: 'bun',
+		// 	rollupConfig: { external: [/^@sentry\//, '@react-pdf/renderer'] },
+		// 	routeRules: {
+		// 		// Cache dynamic-but-slow-changing routes
+		// 		'/activity/**': { swr: 300 }, // 5 min
+		// 		'/clients/**': { swr: 300 }, // 5 min
+		// 		'/dashboard/**': { swr: 300 }, // 5 min
+		// 		'/invoices/**': { swr: 300 }, // 5 min
+		// 		'/memos/**': { swr: 300 }, // 5 min
+		// 		'/settings/**': { swr: 300 }, // 5 min
+		// 		'/team/**': { swr: 300 }, // 5 min
+		// 		'/products/**': { swr: 300 }, // 5 min
+		// 		'/api/**': { swr: 60 }, // 1 min for API routes if needed
+		// 	},
+		// 	// output: {
+		// 	// 	dir: 'dist',
+		// 	// 	serverDir: 'dist/server',
+		// 	// 	publicDir: 'dist/public',
+		// 	// },
+		// }),
 		tailwindcss(),
 		// tanstackRouter({
 		// 	target: 'react',
